70d8ea0a60f0d91853278145c5a5d122bd8a830c
JohnPosada <johnposada2000@gmail.com>
2022-09-22 02:26:28 -0500

    created store with auth reducer and added store to all app routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './AppRouter';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
