21e0d1c5707034e3f49bfa599e3857b5fd3f24d9
JohnPosada <johnposada2000@gmail.com>
2022-09-22 02:13:15 -0500

    added style customization and update colors in index.html file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        LightGrey: '#bebfc5',
+        MediumGrey: '#9da2a4',
+        StrongGrey: '#47494a',
+      },
+      boxShadow: {
+        '3xl': '16px 16px 32px #c8c8c8',
+      },
+    },
   },
   plugins: [],
 };
