913dd2e850fa535db27b3b2349c4b3adb749ebb5
JohnPosada <johnposada2000@gmail.com>
2022-10-20 13:58:13 -0500

    added enviroment variables for firebase config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,14 +5,14 @@ import { getFirestore } from 'firebase/firestore/lite';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyB883iC2jJc0VVbmVZY0bGCFLGZPoUzgxQ',
-  authDomain: 'emburres.firebaseapp.com',
-  projectId: 'emburres',
-  storageBucket: 'emburres.appspot.com',
-  messagingSenderId: '309695268475',
-  appId: '1:309695268475:web:e5de5ed303e7b2b7c884de',
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env
+    .VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
