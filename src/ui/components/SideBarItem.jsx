c3f2a07a3e0f027c2893d877589c606d68f22415
JohnPosada <johnposada2000@gmail.com>
2022-12-07 13:59:36 -0500

    fixed sidebaritem link routing

--- src/ui/components/SideBarItem.jsx
+++ src/ui/components/SideBarItem.jsx
@@ -3,7 +3,10 @@ import { NavLink } from 'react-router-dom';
 export const SideBarItem = ({ title }) => {
   return (
     <>
-      <NavLink className="overflow-hidden w-full text-white hover:bg-gray-600 p-2 rounded font-sans">
+      <NavLink
+        to={title.toLowerCase() === 'inicio' ? '/' : `/${title.toLowerCase()}`}
+        className="overflow-hidden w-full text-white hover:bg-gray-600 p-2 rounded font-sans"
+      >
         {title}
       </NavLink>
     </>
