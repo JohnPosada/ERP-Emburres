8d3b8a3433ef320e076ee609c8800c1ff31b9c2e
JohnPosada <johnposada2000@gmail.com>
2022-12-07 13:53:48 -0500

    declared private routes to supplier module

--- src/router/PrivateRoutes.jsx
+++ src/router/PrivateRoutes.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Header } from '../ui/layout/Header';
 import { SideBar } from '../ui/layout/SideBar';
 import { Footer } from '../ui/layout/Footer';
+import { Route, Routes } from 'react-router-dom';
+import { SuppliersPage } from '../suppliers/page/SuppliersPage';
+import { SuppliersForm } from '../suppliers/component/SuppliersForm';
 
 export const PrivateRoutes = () => {
   return (
@@ -9,9 +12,11 @@ export const PrivateRoutes = () => {
       <Header />
       <div className="flex">
         <SideBar />
-        {/* <Routes>
-              <Route path="*" element={<HomePage />} />
-            </Routes> */}
+        <Routes>
+          <Route path="/proveedores" element={<SuppliersPage />} />
+          <Route path="/proveedores/add" element={<SuppliersForm />} />
+          <Route path="/proveedores/edit/:id" element={<SuppliersForm />} />
+        </Routes>
       </div>
       <Footer />
     </>
